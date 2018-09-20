ce4a4bf4765ed5cc9cb0b73775b17b46c0faee5e
Jake Son <jake.son@childmind.org>
2018-09-19 23:35:02 -0400

    Binary classification using 2/1 split for training and testing, variable accuracy per target and per participant.

--- lstm_analysis.py
+++ lstm_analysis.py
@@ -1,6 +1,7 @@
 # Jake Son
 # Child Mind Institute
 
+import keras
 import numpy as np
 import pandas as pd
 from keras.datasets import imdb
@@ -14,6 +15,8 @@ from sklearn.preprocessing import LabelEncoder
 from keras.wrappers.scikit_learn import KerasClassifier
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import train_test_split
 
 # BINARY CLASSIFICATION TUTORIAL
 
@@ -69,7 +72,7 @@ data = data[(data.ontarget == True)]
 
 data = data.drop(labels=['ontarget'], axis=1)
 
-p1data = data[(data.participant == 1)].drop(labels=['participant'], axis=1)
+p1data = data[(data.participant == 2)].drop(labels=['participant'], axis=1)
 
 p1targets = p1data['target'].values
 p1signals = p1data.drop(labels=['target'], axis=1).values
@@ -82,71 +85,59 @@ encoded_Y = encoder.transform(p1targets)
 dummy_y = np_utils.to_categorical(encoded_Y)
 
 
+# Create binary classification model
 
+df = pd.read_csv('tingle_pilot_data_shareable.csv')
 
+data = df[['distance',
+           'pitch',
+           'roll',
+           'target',
+           'thermopile1',
+           'thermopile2',
+           'thermopile3',
+           'thermopile4',
+           'ontarget',
+           'participant']]
 
+data = data[(data.target == 'rotate-mouth') |
+            (data.target == 'rotate-nose') |
+            (data.target == 'rotate-cheek') |
+            (data.target == 'rotate-eyebrow') |
+            (data.target == 'rotate-top-head') |
+            (data.target == 'rotate-back-head')]
 
+data = data[(data.ontarget == True)]
 
+data = data.drop(labels=['ontarget'], axis=1)
 
+p1data = data[(data.participant == 1)].drop(labels=['participant'], axis=1)
 
+p1targets = list(p1data['target'].values)
+p1targets = np.array([1 if x == 'rotate-back-head' else 0 for x in p1targets])
+p1signals = p1data.drop(labels=['target'], axis=1).values
 
-# Create baseline model for keras multiclass classifier
+x_train, x_test, y_train, y_test = train_test_split(p1signals, p1targets, test_size=.33)
 
-def baseline_model():
-
-    model = Sequential()
-    model.add(Dense(7, input_dim=7, activation='relu'))
-    model.add(LSTM(100, return_sequences=True))
-    # model.add(LSTM(100))
-    model.add(Dense(6, activation='softmax'))
-    # model.compile(loss='sparse_categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    return model
-
-
-estimator = KerasClassifier(build_fn=baseline_model, epochs=20, batch_size=50, verbose=1)
-
-# Set seed for reliability
-seed = 3
-
-kfold = KFold(n_splits=10, shuffle=True, random_state=seed)
-
-results = cross_val_score(estimator, p1signals, dummy_y, cv=kfold)
-print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
-
-
-
-
-
-
-# LSTM for sequence classification in the IMDB dataset
-import numpy
-from keras.datasets import imdb
-from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import LSTM
-from keras.layers.embeddings import Embedding
-from keras.preprocessing import sequence
-# fix random seed for reproducibility
-numpy.random.seed(7)
-# load the dataset but only keep the top n words, zero the rest
-top_words = 5000
-(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=top_words)
-# truncate and pad input sequences
-max_review_length = 500
-X_train = sequence.pad_sequences(X_train, maxlen=max_review_length)
-X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
-# create the model
-embedding_vecor_length = 32
 model = Sequential()
-model.add(Embedding(top_words, embedding_vecor_length, input_length=max_review_length))
-model.add(LSTM(100))
-model.add(Dense(1, activation='sigmoid'))
+model.add(Dense(60, input_dim=7, kernel_initializer='normal', activation='relu'))
+model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
+# Compile model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-print(model.summary())
-model.fit(X_train, y_train, epochs=3, batch_size=64)
-# Final evaluation of the model
-scores = model.evaluate(X_test, y_test, verbose=0)
-print("Accuracy: %.2f%%" % (scores[1]*100))
 
+model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=100, batch_size=32, verbose=1)
 
+# def create_baseline():
+#     # create model
+#     model = Sequential()
+#     model.add(Dense(60, input_dim=7, kernel_initializer='normal', activation='relu'))
+#     model.add(Dense(1, kernel_initializer='normal', activation='sigmoid'))
+#     # Compile model
+#     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+#     return model
+#
+# # evaluate model with standardized dataset
+# estimator = KerasClassifier(build_fn=create_baseline, epochs=10, batch_size=32, verbose=1)
+# kfold = StratifiedKFold(n_splits=2, shuffle=True)
+# results = cross_val_score(estimator, p1signals, p1targets, cv=kfold)
+# print("Results: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
