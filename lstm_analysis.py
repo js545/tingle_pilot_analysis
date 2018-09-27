eea4b1974f163a4c245ceb9bb2a4beb2b367a9b0
Jake Son <jake.son@CMI-RSCH-IM023.local>
2018-09-27 14:22:24 -0400

    Script runs through each participant/target

--- lstm_analysis.py
+++ lstm_analysis.py
@@ -9,7 +9,7 @@ from keras.layers import LSTM
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 
-# TINGLE ANALYSIS
+# Load data and remove columns that are not in use
 
 df = pd.read_csv('tingle_pilot_data_shareable.csv')
 
@@ -23,6 +23,8 @@ data = df[['distance',
            'thermopile4',
            'ontarget',
            'participant']]
+
+# Subset data collected from target locations of interest
 
 data = data[(data.target == 'rotate-mouth') |
             (data.target == 'rotate-nose') |
@@ -40,35 +42,57 @@ data = data[(data.ontarget == True)]
 
 data = data.drop(labels=['ontarget'], axis=1)
 
-p1data = data[(data.participant == 5)].drop(labels=['participant'], axis=1)
+# Subset data from a given participant
 
-p1targets = list(p1data['target'].values)
-p1targets = np.array([1 if x == 'rotate-mouth' else 0 for x in p1targets])
-p1signals = p1data.drop(labels=['target'], axis=1).values
+# for part_num in range(1, len(data.participant.unique()) + 1):
+for part_num in range(1, 3):
 
-x_train, x_test, y_train, y_test = train_test_split(p1signals, p1targets, test_size=.33)
+    # part_num = 1
 
-x_train = x_train.reshape((x_train.shape[0], 1, x_train.shape[1]))
-x_test = x_test.reshape((x_test.shape[0], 1, x_test.shape[1]))
+    p1data = data[(data.participant == part_num)].drop(labels=['participant'], axis=1)
 
-model = Sequential()
-model.add(LSTM(50, input_shape=(x_train.shape[1], x_train.shape[2])))
-model.add(Dense(1, activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    # Select on-target location
 
-history = model.fit(x_train, y_train, epochs=50, batch_size=32, validation_data=(x_test, y_test), verbose=1, shuffle=False)
+    for target_location in ['rotate-mouth', 'rotate-nose', 'rotate-cheek', 'rotate-eyebrow', 'rotate-top-head',
+                            'rotate-back-head']:
 
-sig = model.predict_classes(x_test)
+        # target_location = 'rotate-mouth'
 
-# plot history
-plt.figure()
-plt.plot(history.history['loss'], label='train')
-plt.plot(history.history['val_loss'], label='test')
-plt.legend()
-plt.show()
+        p1targets = list(p1data['target'].values)
+        p1targets = np.array([1 if x == target_location else 0 for x in p1targets])
+        p1signals = p1data.drop(labels=['target'], axis=1).values
 
-plt.figure()
-plt.plot(history.history['acc'], label='train')
-plt.plot(history.history['val_acc'], label='test')
-plt.legend()
-plt.show()
+        # Separate into train/test sets
+
+        x_train, x_test, y_train, y_test = train_test_split(p1signals, p1targets, test_size=.33)
+
+        x_train = x_train.reshape((x_train.shape[0], 1, x_train.shape[1]))
+        x_test = x_test.reshape((x_test.shape[0], 1, x_test.shape[1]))
+
+        # Create LSTM model
+
+        model = Sequential()
+        model.add(LSTM(50, input_shape=(x_train.shape[1], x_train.shape[2])))
+        model.add(Dense(1, activation='sigmoid'))
+        model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+        history = model.fit(x_train, y_train, epochs=50, batch_size=32, validation_data=(x_test, y_test), verbose=1, shuffle=False)
+        # history = model.fit(x_train, y_train, epochs=50, batch_size=32, verbose=1, shuffle=False)
+
+        sig = model.predict_classes(x_test)
+
+        # Plot loss and accuracy
+        # plt.figure()
+        # plt.title(str('Participant {} target {}').format(part_num, target_location))
+        # plt.plot(history.history['loss'], label='train')
+        # plt.plot(history.history['val_loss'], label='test')
+        # plt.legend()
+        # plt.show()
+
+        plt.figure()
+        plt.title(str('Participant {} target {}').format(part_num, target_location))
+        plt.ylim([.5, 1])
+        plt.plot(history.history['acc'], label='train')
+        plt.plot(history.history['val_acc'], label='test')
+        plt.legend()
+        plt.show()
