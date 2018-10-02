6f431a85a8b59b1f772c6c783dccd4edd0b5629a
Jake Son <jake.son@childmind.org>
2018-10-02 11:11:06 -0400

    Cleaned code for manuscript

--- lstm_analysis.py
+++ lstm_analysis.py
@@ -6,38 +6,20 @@ import pandas as pd
 from sklearn import metrics
 from keras.layers import LSTM
 from keras.layers import Dense
-import matplotlib.pyplot as plt
 from keras.models import Sequential
 from sklearn.model_selection import train_test_split
 
-# Load data and remove columns that are not in use
-
 df = pd.read_csv('tingle_pilot_data_shareable.csv')
 
-data = df[['distance',
-           'pitch',
-           'roll',
-           'target',
-           'thermopile1',
-           'thermopile2',
-           'thermopile3',
-           'thermopile4',
-           'ontarget',
-           'participant']]
+data = df[['distance', 'pitch', 'roll', 'target',
+           'thermopile1', 'thermopile2', 'thermopile3', 'thermopile4',
+           'ontarget', 'participant']]
 
-# Subset data collected from target locations of interest
-
-data = data[(data.target == 'rotate-mouth') |
-            (data.target == 'rotate-nose') |
-            (data.target == 'rotate-cheek') |
-            (data.target == 'rotate-eyebrow') |
-            (data.target == 'rotate-top-head') |
-            (data.target == 'rotate-back-head') |
-            (data.target == 'offbody-ceiling') |
-            (data.target == 'offbody-floor') |
-            (data.target == 'offbody-+') |
-            (data.target == 'offbody-X') |
-            (data.target == 'offbody-spiral')]
+data = data[(data.target == 'rotate-mouth') | (data.target == 'rotate-nose') | (data.target == 'rotate-cheek') |
+            (data.target == 'rotate-eyebrow') | (data.target == 'rotate-top-head') |
+            (data.target == 'rotate-back-head') | (data.target == 'offbody-ceiling') |
+            (data.target == 'offbody-floor') | (data.target == 'offbody-+') |
+            (data.target == 'offbody-X') | (data.target == 'offbody-spiral')]
 
 data = data[(data.ontarget == True)]
 
@@ -48,40 +30,30 @@ targets = ['rotate-mouth', 'rotate-nose', 'rotate-cheek', 'rotate-eyebrow', 'rot
 
 results_dict = {}
 
-# Subset data from a given participant
-
 for part_num in range(23, len(data.participant.unique()) + 1):
-
-    print(str('Analyzing participant {}').format(part_num))
 
     p1data = data[(data.participant == part_num)].drop(labels=['participant'], axis=1)
 
-    # Select on-target location
-
     for target_loc in targets:
+
+        print(str('Analyzing participant {} target {}').format(part_num, target_loc))
 
         p1targets = list(p1data['target'].values)
         p1targets = np.array([1 if x == target_loc else 0 for x in p1targets])
         p1signals = p1data.drop(labels=['target'], axis=1).values
-
-        # Separate into train/test sets
 
         x_train, x_test, y_train, y_test = train_test_split(p1signals, p1targets, test_size=.25, shuffle=True)
 
         x_train = x_train.reshape((x_train.shape[0], 1, x_train.shape[1]))
         x_test = x_test.reshape((x_test.shape[0], 1, x_test.shape[1]))
 
-        # Create LSTM model
-
         model = Sequential()
-        # model.add(LSTM(50, input_shape=(x_train.shape[1], x_train.shape[2]), return_sequences=True, dropout=.2))
         model.add(LSTM(50, input_shape=(x_train.shape[1], x_train.shape[2]), return_sequences=True))
         model.add(LSTM(50, dropout=.2))
         model.add(Dense(1, activation='sigmoid'))
         model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
         history = model.fit(x_train, y_train, epochs=50, batch_size=32, validation_data=(x_test, y_test), verbose=0, shuffle=False)
-        # history = model.fit(x_train, y_train, epochs=50, batch_size=32, verbose=1, shuffle=False)
 
         sig = model.predict_classes(x_test)
 
@@ -91,26 +63,6 @@ for part_num in range(23, len(data.participant.unique()) + 1):
         f1score = round(report['weighted avg']['f1-score'], 2)
 
         results_dict[str('{}_{}').format(part_num, target_loc)] = [part_num, target_loc, precision, recall, f1score]
-
-        # conf_matrix = metrics.confusion_matrix(y_test, sig)
-        # print(conf_matrix)
-
-        # Plot loss and accuracy
-        # plt.figure()
-        # plt.title(str('Participant {} target {}').format(part_num, target_location))
-        # plt.plot(history.history['loss'], label='train')
-        # plt.plot(history.history['val_loss'], label='test')
-        # plt.legend()
-        # plt.show()
-
-        # plt.figure()
-        # plt.title(str('Participant {} target {}').format(part_num, target_loc))
-        # plt.ylim([0, 1])
-        # plt.plot(history.history['acc'], label='train')
-        # plt.plot(history.history['val_acc'], label='test')
-        # plt.plot()
-        # plt.legend()
-        # plt.show()
 
 df = pd.DataFrame.from_dict(results_dict, orient='index', columns=['participant', 'target', 'precision', 'recall', 'f1score'])
 df = df.set_index('participant')
