d01a03e9471ab3897c5a9f494f0cf6a44ea3d92c
Jake Son <jake.son@childmind.org>
2018-11-26 10:09:43 -0500

    Updated figure for AUROC Comparison using ggridges

--- tingle_pilot.R
+++ tingle_pilot.R
@@ -4,25 +4,24 @@
 library(mgc)
 library(reshape2)
 library(ggplot2)
+library(ggridges)
 
-plot_mtx <- function(Dx, main.title="Distance Matrix",
-                     xlab.title="Sample Sorted by Source",
-                     ylab.title="Sample Sorted by Source") {
-  data <- melt(Dx)
-  ggplot(data, aes(x=Var1, y=Var2, fill=value)) +
-    geom_tile() +
-    scale_fill_gradientn(name="dist(x, y)",
-                         colours=c("#f2f0f7", "#cbc9e2", "#9e9ac8", "#6a51a3"),
-                         limits=c(min(Dx), max(Dx))) +
-    xlab(xlab.title) +
-    ylab(ylab.title) +
-    theme_bw() +
-    ggtitle(main.title)
-}
+# Ridgeline plots
 
-# DISCRIMINABILITY ANALYSIS
+setwd('/Users/jakeson/Documents/CMI/tingle_pilot_analysis/results')
+n_data = read.csv('auroc_n_thermal.csv')
+y_data = read.csv('auroc_y_thermal.csv')
+n_data$Thermal = c(rep('No thermal', dim(n_data)[1]))
+y_data$Thermal = c(rep('Yes thermal', dim(y_data)[1]))
 
-setwd('/Users/jakeson/Documents/CMI/Tingle Device/')
+data = rbind(n_data, y_data)
+data$thermal = factor(data$Thermal)
+
+ggplot(data, aes(x=AUROC, y=Target, fill=Thermal)) + scale_fill_cyclical(values = c("blue", "red")) + geom_density_ridges(alpha=.5) + ggtitle('AUROC Comparison') + theme(plot.title = element_text(hjust = 0.5))
+
+# Analysis
+
+setwd('/Users/jakeson/Documents/CMI/tingle_pilot_analysis/')
 data = read.csv('tingle_pilot_data_shareable.csv')
 
 vars = c('distance', 'pitch', 'roll',
@@ -43,100 +42,98 @@ mydata <- subset(mydata, target %in% targets)
 
 mydata <- subset(mydata, ontarget == 'True')
 
-# # Participant 1
-# p1data <- subset(mydata, participant == 1)
-# p1data <- droplevels(p1data)
+# # DISCRIMINABILITY ANALYSIS
 # 
-# # Check number of samples per condition
-# table(unlist(p1data$target))
+# # All participants, individual
 # 
-# # Test discriminability score
+# iter <- 50
+# results <- matrix(NA, nrow=iter, ncol=8)
+# colnames(results) = c('Participant', 'Discrim_Score', 'N_back_head', 'N_cheek', 'N_eyebrow', 'N_mouth',
+#                       'N_nose', 'N_top_head')
 # 
-# rownames(p1data) <- 1:nrow(p1data)
-# Dx <- as.matrix(dist(p1data[sort(as.vector(p1data$target),
-#                                  index=TRUE)$ix,c(1,2,3,4,5,6,7)]))
-# plot_mtx(Dx)
+# for (participant_num in unique(mydata$participant)){
+#   
+#   pdata <- subset(mydata, participant == participant_num)
+#   pdata <- droplevels(pdata)
+#   
+#   out <- table(unlist(pdata$target))
+#   back_head = out[1]
+#   cheek = out[2]
+#   eyebrow = out[3]
+#   mouth = out[4]
+#   nose = out[5]
+#   top_head = out[6]
+#   
+#   # discr_score = discr.stat(pdata[,c(1,2,3,4,5,6,7)], as.vector(pdata$target))
+#   discr_score = discr.stat(pdata[,c(1,2,3)], as.vector(pdata$target))
+#   print(discr_score)
+#   
+#   results[participant_num,] = c(participant_num, discr_score, out[1],out[2],out[3],out[4],out[5],out[6])
+#   
+# }
 # 
-# # Calculate discriminability score
-# discr.stat(p1data[,c(1,2,3,4,5,6,7)], as.vector(p1data$target))
-
-# All participants, individual
-
-iter <- 50
-results <- matrix(NA, nrow=iter, ncol=8)
-colnames(results) = c('Participant', 'Discrim_Score', 'N_back_head', 'N_cheek', 'N_eyebrow', 'N_mouth',
-                      'N_nose', 'N_top_head')
-
-for (participant_num in unique(mydata$participant)){
-  
-  pdata <- subset(mydata, participant == participant_num)
-  pdata <- droplevels(pdata)
-  
-  out <- table(unlist(pdata$target))
-  back_head = out[1]
-  cheek = out[2]
-  eyebrow = out[3]
-  mouth = out[4]
-  nose = out[5]
-  top_head = out[6]
-  
-  discr_score = discr.stat(pdata[,c(1,2,3,4,5,6,7)], as.vector(pdata$target))
-  print(discr_score)
-  
-  results[participant_num,] = c(participant_num, discr_score, out[1],out[2],out[3],out[4],out[5],out[6])
-  
-}
-
-# All participants, group
-
-groupdata = droplevels(mydata)
-out <- table(unlist(groupdata$target))
-group_discr_score = discr.stat(groupdata[,c(1,2,3,4,5,6,7)], as.vector(pdata$target))
-
-results[participant_num+1,] = c(100, group_discr_score, out[1],out[2],out[3],out[4],out[5],out[6])
-
-results = na.omit(results)
-
-write.csv(results, file='discr_results.csv')
+# # All participants, group
+# 
+# groupdata = droplevels(mydata)
+# out <- table(unlist(groupdata$target))
+# # group_discr_score = discr.stat(groupdata[,c(1,2,3,4,5,6,7)], as.vector(pdata$target))
+# group_discr_score = discr.stat(groupdata[,c(1,2,3)], as.vector(pdata$target))
+# 
+# results[participant_num+1,] = c(100, group_discr_score, out[1],out[2],out[3],out[4],out[5],out[6])
+# 
+# results = na.omit(results)
+# 
+# write.csv(results, file='discr_results.csv')
 
 # MULTISCALE GRAPH CORRELATION ANALYSIS
 
-dyads <- combn(targets, m=2, simplify=TRUE)
-
-cnames = list()
-
-for (num in 1:dim(dyads)[2]){
-  
-  dyad = paste(dyads[,num], collapse = '_')
-  cnames <- append(cnames, dyad)
-  
-}
-
 iter <- 50
-results <- matrix(NA, nrow=iter, ncol=dim(dyads)[2])
-colnames(results) = cnames
+results <- matrix(NA, nrow=iter, ncol=length(targets))
+colnames(results) = targets
 
-# Participant 1
-
-p1data = subset(mydata, participant == 1)
-
-for (participant_num in unique(mydata$participant)){
-# for (participant_num in 1:2){
+# for (participant_num in unique(mydata$participant)){
+for (participant_num in 23:41) {
+  
+  if (participant_num==22) next
+  
   print(paste(c('Analyzing participant', participant_num), collapse = ' '))
-
-  for (num in 1:dim(dyads)[2]){
+  
+  p1data = subset(mydata, participant == participant_num)
+  tar_int = 1
+  
+  # for (num in 1:dim(dyads)[2]){
+  for (tar in targets){
     
-    prop1 <- subset(p1data, target == dyads[,num][1])
-    prop2 <- subset(p1data, target == dyads[,num][2])
+    print(tar)
     
-    prop1 <- data.matrix(prop1[1:7], rownames.force=NA)
-    prop2 <- data.matrix(prop2[1:7], rownames.force=NA)
+    prop1 <- subset(p1data, target == tar)
+    prop2 <- subset(p1data, target != tar)
     
-    output <- mgc.test(nose, cheek, rep=1000)
+    # prop1 <- data.matrix(prop1[1:7], rownames.force=NA)
+    # prop2 <- data.matrix(prop2[1:7], rownames.force=NA)
+    
+    prop1 <- data.matrix(prop1[1:3], rownames.force=NA)
+    prop2 <- data.matrix(prop2[1:3], rownames.force=NA)
+    
+    rownames(prop1) <- NULL
+    rownames(prop2) <- NULL
+    colnames(prop1) <- NULL
+    colnames(prop2) <- NULL
+    
+    x_data = rbind(prop1, prop2)
+    y_data = rbind(matrix(1, dim(prop1)[1], 1), matrix(0, dim(prop2)[1], 1))
+    
+    # y_data <- y_data[sample(nrow(y_data), nrow(y_data)), ]
+    
+    output <- mgc.test(x_data, y_data, rep=500)
     
     pMGC = output$pMGC
     
-    results[participant_num,num] <- pMGC
+    print(pMGC)
+    
+    results[participant_num,tar_int] <- pMGC
+    
+    tar_int <- tar_int +1
     
   }
   
@@ -144,20 +141,69 @@ for (participant_num in unique(mydata$participant)){
 
 results = na.omit(results)
 
+# head(results, n=3)
+
+write.csv(results, file='binarized_mgc_wo_temp_41.csv')
+
+# # MULTISCALE GRAPH CORRELATION ANALYSIS
+
+dyads <- combn(targets, m=2, simplify=TRUE)
+
+cnames = list()
+
+for (num in 1:dim(dyads)[2]){
+
+  dyad = paste(dyads[,num], collapse = '_')
+  cnames <- append(cnames, dyad)
+
+}
+
+iter <- 50
+results <- matrix(NA, nrow=iter, ncol=dim(dyads)[2])
+colnames(results) = cnames
+
+for (participant_num in unique(mydata$participant)){
+
+# for (participant_num in 1:3){
+  
+  print(paste(c('Analyzing participant', participant_num), collapse = ' '))
+
+  p1data = subset(mydata, participant == participant_num)
+
+  for (num in 1:dim(dyads)[2]){
+  # for (num in 1:5){
+
+    prop1 <- subset(p1data, target == dyads[,num][1])
+    prop2 <- subset(p1data, target == dyads[,num][2])
+
+    prop1 <- data.matrix(prop1[1:3], rownames.force=NA)
+    prop2 <- data.matrix(prop2[1:3], rownames.force=NA)
+    
+    rownames(prop1) <- NULL
+    rownames(prop2) <- NULL
+    colnames(prop1) <- NULL
+    colnames(prop2) <- NULL
+    
+    x_data = rbind(prop1, prop2)
+    y_data = rbind(matrix(1, dim(prop1)[1], 1), matrix(0, dim(prop2)[1], 1))
+
+    output <- mgc.test(x_data, y_data, rep=1000)
+
+    pMGC = output$pMGC
+    
+    print(c(dyads[,num], pMGC))
+
+    results[participant_num,num] <- pMGC
+
+  }
+
+}
+
+results = na.omit(results)
+
 head(results, n=3)
 
-write.csv(results, file='mgc_results.csv')
-
-# NEURAL NETWORK ANALYSIS
-
-library(kerasR)
-library(reticulate)
-
-use_python("/Users/jakeson/anaconda3/bin/python3.6")
-keras_init()
-keras_available()
-
-
+write.csv(results, file='1000_mgc_results_n_thermal.csv')
 
 
 
